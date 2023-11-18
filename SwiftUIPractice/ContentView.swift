3f2f65c5187aa12915ac71f4d58f9f00bbc72875
Shraddha <shraddhasutar28@gmail.com>
2023-11-18 20:11:02 +0530

    Text component practice

--- SwiftUIPractice/ContentView.swift
+++ SwiftUIPractice/ContentView.swift
@@ -9,14 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack(alignment: .center) {
-            Color.red
-            VStack(alignment: .center){
-                Text("Hello, world!")
-                    .background(.green)
-            }
+       VStack(alignment: .center, spacing: 20) {
+           Text("Hello, world!")
+               .frame(maxWidth: .infinity, maxHeight: 100)
+               .background(.red)
+               .foregroundColor(.secondary)
+               .font(.largeTitle)
+           
+           Text("Hello, world!")
+               .frame(maxWidth: .infinity, maxHeight: 100)
+               .background(.white)
+               .foregroundColor(.secondary)
+               .font(.title3)
+           
+           Text("Hello, world!")
+               .frame(maxWidth: .infinity, maxHeight: 100)
+               .background(.green)
+               .foregroundColor(.secondary)
+               .font(Font.system(size: 40, weight: .bold))
+               
         }
-        .background(.blue)
+       .frame(maxWidth: .infinity, maxHeight: .infinity)
+       .background(LinearGradient(colors: [Color.red, Color.white, Color.green], startPoint: .top, endPoint: .bottom))
         .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
